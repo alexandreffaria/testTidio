a9835c4d8a6f2d922e4cfae423997b621d496d10
Alexandre Faria <alexandreffaria@me.com>
2024-06-04 12:55:19 -0300

    better naming

--- runAllProductTests.ps1
+++ runAllProductTests.ps1
@@ -1,6 +1,8 @@
 # Define the values for each product
-$colorValues = 0, 8, 16, 24
-$bookValues = 0, 5, 10, 15
+# $colorValues = 0, 8, 16, 24
+# $bookValues = 0, 5, 10, 15
+$colorValues = 0, 8
+$bookValues = 0, 5
 
 # Loop through all combinations of values for each product
 foreach ($azul in $colorValues) {
